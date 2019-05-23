585ff78656a02ea860d8d129c9c7fd30b3c0730f
Eric Kofman <ericrkofman@gmail.com>
2019-05-23 11:32:20 -0400

    remove plotting of sample level coverage

--- process_coverage_info.py
+++ process_coverage_info.py
@@ -115,27 +115,8 @@ def generate_sample_mean_coverage_data(sample_folder_path, cutoff, cohort_label,
         if sample_id:
             mean_coverages.append(sample_mean_coverage)
             sample_id_to_mean_coverage[sample_id] = sample_mean_coverage
-    """
-    sys.stdout.write('Plotting histograms for sample level mean coverage\n')
-    # Make a histogram of all samples in the cohort
-    plt.hist(np.array(mean_coverages), bins=50, histtype='step')
-    plt.axvline(x=cutoff, color='r')
-    plt.title('Sample Mean Coverage Distribution\n{} Cohort'.format(cohort_label))
-    plt.ylabel('Samples')
-    plt.xlabel('Sample Mean Coverage')
-    plt.savefig('{}/{}_sample_mean_coverage'.format(output_folder, cohort_label.replace(' ', '_')))
+            sys.stdout.write('{}\t{}\n'.format(sample_id, sample_mean_coverage))
 
-    plt.figure()
-    # Make a histogram of the ones that are lower than the cutoff
-    lower_than_cutoff = [v for v in mean_coverages if v < cutoff]
-    plt.hist(np.array(lower_than_cutoff), bins=50, histtype='step')
-    plt.axvline(x=cutoff, color='r')
-    plt.title('Sample Mean Coverage Distribution for Samples below Cutoff of {}:\n{} Cohort'.format(cutoff,
-                                                                                                   cohort_label))
-    plt.ylabel('Samples')
-    plt.xlabel('Sample Mean Coverage')
-    plt.savefig('{}/{}_sample_mean_coverage_below_cutoff'.format(output_folder, cohort_label.replace(' ', '_')))
-    """
     sys.stdout.write("Writing sample level mean coverage output\n")
     # Output list of all samples with corresponding mean sample coverage and whether they passed the coverage cutoff
     with open('{}/{}_mean_coverage_by_sample_id.tsv'.format(output_folder, cohort_label), 'w') as f:
